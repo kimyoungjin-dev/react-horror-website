ae105005d51be1ad31d3f075019328d046bad9ff
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-04-03 22:59:57 +0900

    Create Hero.js

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,7 +1,31 @@
 import React from "react";
+import styled from "styled-components";
+
+const HeroSection = styled.section`
+  height: 100vh;
+  max-height: 1100px;
+  position: relative;
+  overflow: hidden;
+`;
+
+const HeroWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+  height: 100%;
+  width: 100%;
+  position: relative;
+`;
 
 const Hero = () => {
-  return <div></div>;
+  return (
+    <HeroSection>
+      <HeroWrapper>
+        <h1>hello</h1>
+      </HeroWrapper>
+    </HeroSection>
+  );
 };
 
 export default Hero;
