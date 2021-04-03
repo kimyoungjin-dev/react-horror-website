541c85c14a298a66deff0a4df168e14e7f335eb9
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-04-04 01:35:45 +0900

    Passing isOpen argument to Hero.js

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -1,8 +1,8 @@
 import React from "react";
 import Hero from "Components/Hero";
 
-const Home = () => {
-  return <Hero />;
+const Home = ({ isOpen }) => {
+  return <Hero isOpen={isOpen} />;
 };
 
 export default Home;
