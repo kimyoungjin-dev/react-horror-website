e2aa39be52c55407c13c1911da0c4135d8d8b3dd
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-04-04 03:26:24 +0900

    Pass infoData to InfoSection.js on the home screen.

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -1,8 +1,15 @@
 import React from "react";
 import Hero from "Components/Hero";
+import InfoSection from "Components/InfoSection";
+import { InfoData, InfoDataTwo } from "data/InfoData";
 
 const Home = ({ isOpen }) => {
-  return <Hero isOpen={isOpen} />;
+  return (
+    <>
+      <Hero isOpen={isOpen} />
+      <InfoSection {...InfoData} isOpen={isOpen} />
+    </>
+  );
 };
 
 export default Home;
