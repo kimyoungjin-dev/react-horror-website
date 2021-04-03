1cd169712c6dad24c3182b4a362b9555deb43e99
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-04-04 03:25:55 +0900

    Create InfoData and export it.

---
+++ src/data/InfoData.js
@@ -0,0 +1,14 @@
+import ImageOne from "images/house-1.webp";
+import ImageTwo from "images/house-4.webp";
+
+export const InfoData = {
+  heading: "The scariest house of horror in the world",
+  paragraphOne:
+    "It is the scariest house of horror in the world. You shouldn't come here.",
+  paragraphTwo:
+    "It is the scariest house of horror in the world. You shouldn't come hereIt is the scariest house of horror in the world. You shouldn't come here.It is the scariest house of horror in the world. You shouldn't come here.",
+  buttonLabel: "View Homes",
+  image: ImageOne,
+  reverse: false,
+  delay: 100,
+};
