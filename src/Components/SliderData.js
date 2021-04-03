9b2b294954f9a02bcd843a4750b2dc9ce9fc7ce7
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-04-03 23:00:20 +0900

    Create a component that has photo information and title information.

---
+++ src/Components/SliderData.js
@@ -0,0 +1,89 @@
+import Imageone from "images/house-1";
+import ImageTwo from "images/house-2";
+import ImageThree from "images/house-3";
+import ImageFour from "images/house-4";
+
+export const SliderData = [
+  {
+    title: "Ranked 1st in the world's scariest place",
+    place: "$4,280,000",
+    path: "/home",
+    label: "Rank 1",
+    image: Imageone,
+    alt: "House",
+  },
+
+  {
+    title: "Ranked 2st in the world's scariest place",
+    price: "$4, 280,000",
+    path: "/home",
+    label: "View Home",
+    image: ImageTwo,
+    alt: "House",
+  },
+
+  {
+    title: "Ranked 3st in the world's scariest place",
+    price: "$4, 280,000",
+    path: "/home",
+    label: "View Home",
+    image: ImageThree,
+    alt: "House",
+  },
+
+  {
+    title: "Ranked 4st in the world's scariest place",
+    price: "$4, 280,000",
+    path: "/home",
+    label: "View Home",
+    image: ImageFour,
+    alt: "House",
+  },
+];
+
+export const SliderDataTwo = [
+  {
+    title: "Ranked 5st in the world's scariest place",
+    price: "$4, 280,000",
+    path: "/home",
+    label: "View Home",
+    image: ImageFive,
+    alt: "House",
+  },
+
+  {
+    title: "Ranked 7st in the world's scariest place",
+    price: "$4, 280,000",
+    path: "/home",
+    label: "View Home",
+    image: ImageSix,
+    alt: "House",
+  },
+
+  {
+    title: "Ranked 8st in the world's scariest place",
+    price: "$4, 280,000",
+    path: "/home",
+    label: "View Home",
+    image: ImageSeven,
+    alt: "House",
+  },
+
+  {
+    title: "Ranked 6st in the world's scariest place",
+    price: "$4, 280,000",
+    path: "/home",
+    label: "View Home",
+    image: ImageEight,
+    alt: "House",
+  },
+
+  {
+    title: "Ranked 9st in the world's scariest place",
+    price: "$4, 280,000",
+    path: "/home",
+    label: "View Home",
+    image: ImageNine,
+    alt: "House",
+  },
+];
