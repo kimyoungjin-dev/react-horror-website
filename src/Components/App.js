9e23a13eb8739d3838337d09b3ea51779157e170
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-04-03 21:50:04 +0900

    Import the Hero component from App.js.

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import GlobalStyles from "Components/GlobalStyles";
 import Router from "Components/Router";
+import Hero from "Components/Hero";
 
 const App = () => {
   return (
     <>
       <GlobalStyles />
       <Router />
+      <Hero />
     </>
   );
 };
