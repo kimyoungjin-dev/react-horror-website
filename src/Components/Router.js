a0ebe39805d2ab1010549876621fc62c562a2acb
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-04-04 03:23:14 +0900

    Clean up the import path

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -4,17 +4,17 @@ import {
   Route,
   Redirect,
 } from "react-router-dom";
-
 import Header from "Components/Header";
 import Home from "Screens/Home";
 import About from "Screens/About";
 import Rentals from "Screens/Rentals";
-import Dropdown from "./Dropdown";
+import Dropdown from "Components/Dropdown";
 
 export default ({ toggle, isOpen }) => (
   <Router>
     <Header toggle={toggle} isOpen={isOpen} />
     <Dropdown toggle={toggle} isOpen={isOpen} />
+
     <Switch>
       <Route exact path="/" component={Home}>
         <Home isOpen={isOpen} />
