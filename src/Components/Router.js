d924804c0e481ea7aaee1f480d40d3f69783db76
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-04-03 21:02:04 +0900

    Add a screen to the router

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -5,12 +5,18 @@ import {
   Redirect,
 } from "react-router-dom";
 import Header from "Components/Header";
+import Home from "Screens/Home";
+import About from "Screens/About";
+import Rentals from "Screens/Rentals";
 
 export default () => (
   <Router>
     <Header />
     <Switch>
-      <Route />
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/rentals" component={Rentals} />
+
       <Redirect from="*" to="/" />
     </Switch>
   </Router>
