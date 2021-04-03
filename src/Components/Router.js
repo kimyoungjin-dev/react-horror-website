1c1e739d50fc58c59dfbf1bcd912eb300609caef
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-04-04 01:35:29 +0900

    Pass arguments to each screen

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -4,16 +4,21 @@ import {
   Route,
   Redirect,
 } from "react-router-dom";
+
 import Header from "Components/Header";
 import Home from "Screens/Home";
 import About from "Screens/About";
 import Rentals from "Screens/Rentals";
+import Dropdown from "./Dropdown";
 
-export default () => (
+export default ({ toggle, isOpen }) => (
   <Router>
-    <Header />
+    <Header toggle={toggle} isOpen={isOpen} />
+    <Dropdown toggle={toggle} isOpen={isOpen} />
     <Switch>
-      <Route exact path="/" component={Home} />
+      <Route exact path="/" component={Home}>
+        <Home isOpen={isOpen} />
+      </Route>
       <Route path="/about" component={About} />
       <Route path="/rentals" component={Rentals} />
 
