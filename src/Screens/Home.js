8dafebce9cc7cdfbee4e2fa33417a545189799e7
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-04-04 14:38:49 +0900

    Finally, render in home.js.

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -1,13 +1,27 @@
 import React from "react";
 import Hero from "Components/Hero";
-import InfoSection from "Components/InfoSection";
-import { InfoData } from "data/InfoData";
+import InfoSection from "Components/InfoSection/InfoSection";
+import InfoSectionTwo from "Components/InfoSectionTwo";
+import InfoSectionThree from "Components/InfoSection/InfoSectionThree";
+import {
+  InfoData,
+  InfoDataTwo,
+  InfoDataThree,
+  InfoDataFour,
+  InfoDataFive,
+} from "data/InfoData";
+import InfoSectionFour from "Components/InfoSection/InfoSectionFour";
+import InfoSectionFive from "Components/InfoSection/InfoSectionFive";
 
 const Home = ({ isOpen }) => {
   return (
     <>
       <Hero isOpen={isOpen} />
       <InfoSection {...InfoData} isOpen={isOpen} />
+      <InfoSectionTwo {...InfoDataTwo} isOpen={isOpen} />
+      <InfoSectionThree {...InfoDataThree} isOpen={isOpen} />
+      <InfoSectionFour {...InfoDataFour} isOpen={isOpen} />
+      <InfoSectionFive {...InfoDataFive} isOpen={isOpen} />
     </>
   );
 };
